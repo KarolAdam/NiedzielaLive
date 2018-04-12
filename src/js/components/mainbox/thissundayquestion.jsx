06afad9780f96ca211b9a99cf12442c0a77657ec
Karolina Adamiuk <k.adamiuk@gmail.com>
2018-04-12 17:30:28 +0200

    minor changes

--- src/js/components/mainbox/thissundayquestion.jsx
+++ src/js/components/mainbox/thissundayquestion.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default class ThisSundayQuestion extends React.Component {
     render() {
-        return <h1 className='this-sunday-question'>Czy w najbliższą niedziele zrobię zakupy?</h1>
+        return <h1 className='this-sunday-question'>Czy w najbliższą niedzielę zrobię zakupy?</h1>
     }
 }
