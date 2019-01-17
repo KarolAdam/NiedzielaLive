ff708fa69dc3a40d6db2b70502110b4b3ba45406
Karolina Adamiuk <k.adamiuk@gmail.com>
2019-01-17 20:00:55 +0100

    added cookie info 🍪

--- src/js/components/calendar/calendarheader.jsx
+++ src/js/components/calendar/calendarheader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default class CalendarHeader extends React.Component {
     render() {
-        return <span className='calendar calendar-header'>Kalendarz niedziel wolnych od handlu i świąt:</span>
+        return <span className='calendar calendar-header'>Kalendarz dni wolnych od handlu:</span>
 
     }
 }
